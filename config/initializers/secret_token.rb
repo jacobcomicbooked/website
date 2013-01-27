5f17e6f60ffbf71ca529403add8c59dc6bed7f4c
Jacob Thomas <jacobthomas@comicbooked.com>
2013-01-27 15:47:33 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = '651cda595f9f4b5277d7d318104a820c9639cfe03af5e67c49fd5f389e3fca0fd5ed8055733dc941adde7650246d14587abf20749dee71c72acf62b18c7f053a'
